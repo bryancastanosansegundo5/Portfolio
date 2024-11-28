ea3ddc648a6226a9e94962035385f90f0e56937f
bryancastanosansegundo5 <bryanlopesrider100@gmail.com>
2024-11-28 18:46:06 +0100

    v2.1.1 desplegada

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// index.js
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
